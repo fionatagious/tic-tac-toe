df0d51db84a61bcb818afdcc0cb697bbc6acdf8f
fionatagious <ftang@codeforamerica.org>
2023-10-10 10:03:07 -0500

    WIP board and update scores

--- src/App.js
+++ src/App.js
@@ -1,22 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  function checkSubset(parentArray, subsetArray) {
+    return subsetArray.every((el) => {
+      return parentArray.includes(el);
+    });
+  }
+
+  const [numbersA, setNumbersA] = useState([]);
+  const [numbersB, setNumbersB] = useState([]);
+  const [turnsLeft, setTurnsLeft] = useState(8);
+  const [marker, setMarker] = useState("X");
+  const [playerA, setPlayerA] = useState(true);
+
+  function updatePlayerSquares(squareId) {
+    function setNumbersA(numbersA) {
+      numbersA.push(...squareIdArray);
+    }
+    function setNumbersB(numbersB) {
+      numbersB.push(...squareIdArray);
+    }
+    const squareIdArray = [squareId];
+
+    if (playerA) {
+      setNumbersA(numbersA);
+    } else {
+      setNumbersB(numbersB);
+    }
+    const integersArrayA = numbersA.map((str) => {
+      return parseInt(str);
+    });
+    const integersArrayB = numbersB.map((str) => {
+      return parseInt(str);
+    });
+    return [integersArrayA, integersArrayB];
+  }
+
+  function checkForWinner(a, b) {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winConditions.map((conditionArray) => {
+      return checkSubset(a, conditionArray);
+    });
+    winConditions.map((conditionArray) => {
+      return checkSubset(b, conditionArray);
+    });
+  }
+
+  const handleClick = (e) => {
+    setMarker(marker === "X" ? "O" : "X");
+    e.currentTarget.innerHTML = marker;
+
+    // update player squares
+    const [aSquares, bSquares] = updatePlayerSquares(e.currentTarget.id);
+    console.log("scores", [aSquares, bSquares]);
+
+    // check for win condition
+    if (checkForWinner(aSquares, bSquares)) {
+      console.log("win condition met");
+    } else {
+      console.log("no win");
+    }
+
+    // switch players and subtract 1 turn
+    setPlayerA(!playerA);
+    setTurnsLeft(turnsLeft - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Tic Tac Toe!</p>
+        <div className="game-board">
+          <div id="0" className="square" onClick={handleClick}></div>
+          <div id="1" className="square" onClick={handleClick}></div>
+          <div id="2" className="square" onClick={handleClick}></div>
+          <div id="3" className="square" onClick={handleClick}></div>
+          <div id="4" className="square" onClick={handleClick}></div>
+          <div id="5" className="square" onClick={handleClick}></div>
+          <div id="6" className="square" onClick={handleClick}></div>
+          <div id="7" className="square" onClick={handleClick}></div>
+          <div id="8" className="square" onClick={handleClick}></div>
+        </div>
       </header>
     </div>
   );
